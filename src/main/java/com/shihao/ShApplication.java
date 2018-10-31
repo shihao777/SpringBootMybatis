ca35a992ca6cf460bedf4df27a4c6fd6c7a40c55
FXSBEMEEKEQ89GV\Administrator <shihao_0613@163.com>
2018-10-31 15:19:28 +0800

    提交代码

---
+++ src/main/java/com/shihao/ShApplication.java
@@ -0,0 +1,12 @@
+package com.shihao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShApplication.class, args);
+	}
+}
